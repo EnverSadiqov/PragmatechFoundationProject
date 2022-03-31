2ce23c08cf3cf31c53a71be7ff827bcc9fde5de2
sadiqov enver <b-l-i-m@mail.ru>
2022-03-31 20:50:47 +0400

    tapsiriq yeni

---
+++ Python2/tapsiriq.py
@@ -0,0 +1,48 @@
+# arr=[1,34,23,90,230,1400,600,300,240] bu listi nəzərə alaraq aşağıdakı problemləri həlledin
+# (2) Ən böyük 3 ədədi ekrana çap edin
+# (3) Ədədlərin ortalamasından böyük olan ədədləri ekrana çap edin
+# (4) 2 rəqəmli ədədləri ekrana çap edin.
+# (5) Bu listin içərisində olan ədədlərin rəqəmlərinin cəmini ekrana çap edin (nüm: 34->3+4=7)
+# countries=['Azərbaycan','Turkiyə','Polsa','Macaristan','Ingiltere','Gurcustan']
+# (7) Bütün ölkələrin hərf sayını tapın Link
+# (8)  Ən uzun ölkə adını tapın
+# (9)  Daxilində A hərfi olan ölkələri tapın
+
+ # Ən böyük 3 ədədi ekrana çap edin: 2-ci tapsiriq hell oldu
+# liste = [1,34,23,90,230,1400,600,300,240] 
+# liste.sort() 
+
+# print("1. Büyük Sayı:", liste[-1])
+# print("2. Büyük Sayı:", liste[-2])
+# print("3. Büyük Sayı:", liste[-3])
+
+# Ədədlərin ortalamasından böyük olan ədədləri ekrana çap edin : 3cu tapsiriq hell oldu
+# arr=[1,34,23,90,230,1400,600,300,240]
+# print (sum(arr))
+# print(2918//9)
+
+#####2 rəqəmli ədədləri ekrana çap edin :4cu hell olunmayib
+
+
+# #Bu listin içərisində olan ədədlərin rəqəmlərinin cəmini ekrana çap edin :5ci tapsiriq hell oldu
+# arr=[1,34,23,90,230,1400,600,300,240]
+# print (sum(arr))
+
+##### Bütün ölkələrin hərf sayını tapın:7 ci tapsiriq hell olunmayib
+
+# Ən uzun ölkə adını tapın: 8ci tapsiriq hell 
+# 
+# countries=['Azərbaycan','Turkiyə','Polsa','Macaristan','Ingiltere','Gurcustan']
+# print(max(countries, key=len))
+
+#  Daxilində A hərfi olan ölkələri tapın :9ci  tapsiriq  hell oldu
+    
+# countries= ("Azərbaycan','Turkiyə','Polsa','Macaristan','Ingiltere','Gurcustan")
+# x = countries.count("a")
+# for x in countries:
+#     z = countries.count("a")
+#     x = countries.count("A")
+# print(x)  
+# print(z) 
+
+
